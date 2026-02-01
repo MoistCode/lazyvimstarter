e03d67d504ceeeb992fa7e9162641961447f8aa2
Tommy Pham <tommy.pham@hungryroot.com>
2026-02-01 14:44:23 -0500

    configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.scrolloff = 15
+
+-- Change cwd to the directory passed as an argument
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+      vim.cmd.cd(arg)
+    end
+  end,
+})
